cb26e427681aee3b707df664824fdd4a8977e859
Johnathan Howell <me@johnathanhowell.com>
2016-11-30 17:35:48 -0500

    remove debugging echos

--- build.sh
+++ build.sh
@@ -4,10 +4,8 @@ LANGS=( "de" "fr" "ru" "zh-hans" "zh-hant" )
 
 for file in $(find ./content -name '*.html')
 do
-	echo $file
 	for lang in ${LANGS[@]}
 	do
-		echo $lang
 		cp $file ${file%.html}.$lang.html
 	done
 done
