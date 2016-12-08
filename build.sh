dcac1aa2930f1ec1dbefce71036df06a31cd13b2
Johnathan Howell <me@johnathanhowell.com>
2016-12-07 21:57:25 -0500

    add /download, /downloads page aliases

--- build.sh
+++ build.sh
@@ -4,7 +4,19 @@ LANGS=( "de" "fr" "ru" "zh-hans" "zh-hant" )
 
 for oldfile in $(find ./content -name '*.*.html')
 do
-	rm $oldfile
+	rm -f $oldfile
+done
+
+rm -f ./content/download.html
+rm -f ./content/downloads.html
+
+for file in $(find ./content -name '*.html')
+do
+	if [ "$(basename "${file}")" = "apps.html" ];
+	then
+		cp $file $(dirname ${file})/download.html
+		cp $file $(dirname ${file})/downloads.html
+	fi
 done
 
 for file in $(find ./content -name '*.html')
